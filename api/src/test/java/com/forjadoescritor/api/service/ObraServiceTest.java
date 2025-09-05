291fc3ccc602ec06594d28f71fca8d6edf5a4fba
AlbertoJPS <albertojps.psn@outlook.com>
2025-09-05 19:51:55 -0300

    Update ObraServiceTest.java
    Refeitos os testes no ObraServiceTeste.java usando Mockito  para dublar o banco de dados e evitar interações desnecessárias com o banco de dados real.

--- api/src/test/java/com/forjadoescritor/api/service/ObraServiceTest.java
+++ api/src/test/java/com/forjadoescritor/api/service/ObraServiceTest.java
@@ -2,29 +2,52 @@ package com.forjadoescritor.api.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import java.util.UUID;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import com.forjadoescritor.api.Obra;
+import com.forjadoescritor.api.model.Obra;
+import com.forjadoescritor.api.repository.ObraRepository;
 
+@ExtendWith(MockitoExtension.class)
 public class ObraServiceTest {
+
+    @Mock
+    private ObraRepository obraRepositoryMock; // 1. Criamos o "dublê" do repositório
+
+    @InjectMocks
+    private ObraService obraService; // 2. Injetamos o dublê no nosso serviço real
 
     @Test
     @DisplayName("Deve criar obra com sucesso quando dados são válidos")
-    void criar_ComObraValida_DeveRetornarObraComId() {
+    void criar_ComObraValida_DeveRetornarObraSalva() {
         // Arrange (Preparação)
-        ObraService obraService = new ObraService();
         Obra obraParaSalvar = new Obra();
         obraParaSalvar.setTitulo("Título de Teste");
         obraParaSalvar.setAutor("Autor de Teste");
+
+        Obra obraRetornadaPeloRepo = new Obra();
+        obraRetornadaPeloRepo.setId(UUID.randomUUID());
+        obraRetornadaPeloRepo.setTitulo(obraParaSalvar.getTitulo());
+        obraRetornadaPeloRepo.setAutor(obraParaSalvar.getAutor());
+
+        // "Treinando" o dublê: Quando o método save for chamado com QUALQUER Obra...
+        when(obraRepositoryMock.save(any(Obra.class))).thenReturn(obraRetornadaPeloRepo);
 
         // Act (Ação)
         Obra obraSalva = obraService.criar(obraParaSalvar);
 
         // Assert (Verificação)
         assertNotNull(obraSalva);
-        assertNotNull(obraSalva.getId(), "O ID não deveria ser nulo após a criação");
-        assertEquals("Título de Teste", obraSalva.getTitulo(), "O título deveria ser o mesmo que foi enviado");
+        assertNotNull(obraSalva.getId());
+        assertEquals("Título de Teste", obraSalva.getTitulo());
     }
 }
